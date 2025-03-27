cc0f182eb8fdd9fb0009ce8f7e0ecbd6094ddd54
Beddy724 <jadelight9724@gmail.com>
2025-03-27 19:24:12 +0900

    plywright.config.ts 파일 수정(Edge)

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,22 +4,21 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   use: {
-    headless: true, // true로 설정해야 깃헙 액션스에서 실행 가능
-    viewport: { width: 1500, height: 1200 },
+    headless: true, // true로 설정해야 GitHub Actions에서 실행 가능 (깃헙 액션스는 GUI가 없는 서버 환경이기 떄문)
+    viewport: { width: 1280, height: 720 },
     actionTimeout: 0,
     screenshot: 'only-on-failure',
   },
   projects: [
     {
       name: 'Chromium (Chrome)',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], headless: true },
     },
     {
       name: 'Microsoft Edge',
       use: {
-        channel: 'msedge', // 엣지 브라우저
-        viewport: { width: 1500, height: 1200 },
-        headless: false,
+        channel: 'msedge',
+        headless: true, // Edge도 수정
       },
     },
   ],
