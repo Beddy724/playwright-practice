9d5246d22f219e0beb8d789755921299da0f4d48
Beddy724 <jadelight9724@gmail.com>
2025-03-27 22:41:32 +0900

    컨피그 파일 스크린샷 성공 코드 수정

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,10 @@ export default defineConfig({
   testDir: './tests',
   timeout: 90000,
   use: {
-    headless: true, // true로 설정해야 GitHub Actions에서 실행 가능 (깃헙 액션스는 GUI가 없는 서버 환경이기 때문)
+    headless: true, // true로 설정해야 GitHub Actions에서 실행 가능 (깃헙 액션스는 GUI가 없는 서버 환경이기 떄문)
     viewport: { width: 1280, height: 720 },
     actionTimeout: 0,
-    screenshot: 'only-on-failure', // ✅ 실패 시 스크린샷 저장
-    video: 'on', // ✅ 모든 테스트 비디오 저장
+    screenshot: 'on',
   },
   projects: [
     {
