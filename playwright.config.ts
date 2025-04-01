6daece91b4c5a9560ad1dfdc5d55c54805b722c8
Beddy724 <jadelight9724@gmail.com>
2025-04-01 13:43:54 +0900

    완벽 수정 4월 1일

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 150000, // 2분 30초
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -14,8 +15,8 @@ export default defineConfig({
 
   use: {
     headless: false,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    screenshot: 'on',  // 성공,실패 상관없이 스크린샷 저장 (실패한 테스트만 찍고 싶을 경우 "only-on-failure")
+    video: 'on',     // 성공,실패 상관없이 비디오 저장 (실패한 테스트만 찍고 싶을 경우 "retain-on-failure")
     launchOptions: {
     slowMo: 1000,
     }
@@ -26,16 +27,16 @@ export default defineConfig({
       name: 'chromium',
       use: { 
         ...devices['Desktop Chrome'],
-        screenshot: 'only-on-failure',
-        viewport: { width: 1920, height: 2000 }
+        screenshot: 'on',
+        viewport: { width: 1920, height: 1500 }
       },
     },
     {
       name: 'edge',
       use: { 
         ...devices['Desktop Edge'],
-        screenshot: 'only-on-failure',
-        viewport: { width: 1920, height: 2000 }
+        screenshot: 'on',
+        viewport: { width: 1920, height: 1500 }
       },
     }
   ],
