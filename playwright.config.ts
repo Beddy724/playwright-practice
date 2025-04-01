2ae922485b6247bb65b9ba43772bd5ada3e787db
Beddy724 <jadelight9724@gmail.com>
2025-04-01 14:04:12 +0900

    컨피그 파일 재수정

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,30 +2,33 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 150000, // 2분 30초
+  timeout: 150000, // 전체 테스트 타임아웃
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
 
-  // html 결과 보여주기
+  // ✅ HTML 리포트 설정
   reporter: [
     ['html', { outputFolder: 'playwright-report' }]
   ],
 
+  // ✅ 테스트 결과 파일 저장 경로
+  outputDir: 'test-results',
+
   use: {
     headless: false,
-    screenshot: 'on',  // 성공,실패 상관없이 스크린샷 저장 (실패한 테스트만 찍고 싶을 경우 "only-on-failure")
-    video: 'on',     // 성공,실패 상관없이 비디오 저장 (실패한 테스트만 찍고 싶을 경우 "retain-on-failure")
+    screenshot: 'on',           // 성공/실패 상관없이 스크린샷 저장
+    video: 'on',                // 성공/실패 상관없이 비디오 저장
     launchOptions: {
-    slowMo: 1000,
+      slowMo: 1000,             // 느리게 보기
     }
   },
 
   projects: [
     {
       name: 'chromium',
-      use: { 
+      use: {
         ...devices['Desktop Chrome'],
         screenshot: 'on',
         viewport: { width: 1920, height: 1300 }
@@ -33,7 +36,7 @@ export default defineConfig({
     },
     {
       name: 'edge',
-      use: { 
+      use: {
         ...devices['Desktop Edge'],
         screenshot: 'on',
         viewport: { width: 1920, height: 1300 }
