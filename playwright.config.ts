e1fc5d8c60b59ca242477b90b1b15a034c32eaba
Beddy724 <jadelight9724@gmail.com>
2025-04-03 17:42:18 +0900

    재수정

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,19 +8,18 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
 
-  // ✅ HTML 리포트 설정 (리포트 자동 열지 않음)
   reporter: [
-    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+    ['html', { outputFolder: 'playwright-report'}]
   ],
 
+  // ✅ 여기에는 outputDir 빼야 오류 안 남
   use: {
-    headless: true,                     // CI 환경용
-    screenshot: 'on',                   // 모든 테스트 결과 스크린샷 저장
-    video: 'on',                        // 모든 테스트 결과 비디오 저장
+    headless: true,
+    screenshot: 'on',  // always 저장
+    video: 'on',       // always 저장
     launchOptions: {
-      slowMo: 1000                      // 느리게 실행 (디버깅 용이)
-    },
-    trace: 'retain-on-failure'         // 실패 시 trace 유지
+      slowMo: 1000,   // 느리게 실행 (디버깅 용이)
+    }
   },
 
   projects: [
@@ -28,18 +27,19 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        viewport: { width: 1920, height: 1300 }
+        viewport: { width: 1920, height: 1300 },
       },
     },
     {
       name: 'edge',
       use: {
         ...devices['Desktop Edge'],
-        viewport: { width: 1920, height: 1300 }
+        viewport: { width: 1920, height: 1300 },
       },
     }
   ],
 });
+
 
 
 
